a0c48dd9cd00642d1854cd7c2cd5275e5c778ef7
Mouad Halli <mhalli@e2r11p2.1337.ma>
2021-05-14 16:11:16 +0000

    update

--- setup.sh
+++ setup.sh
@@ -5,13 +5,13 @@ minikube addons enable metallb
 eval $(minikube -p minikube docker-env)
 
 #build images
-docker build -t local-nginx srcs/nginx
-docker build -t local-mysql srcs/mysql
-docker build -t local-phpmyadmin srcs/phpmyadmin
-docker build -t local-wordpress srcs/wordpress
-docker build -t local-ftps srcs/ftps
-docker build -t local-influxdb srcs/influxdb
-docker build -t local-grafana srcs/grafana
+docker build -t nginx srcs/nginx
+docker build -t mysql srcs/mysql
+docker build -t phpmyadmin srcs/phpmyadmin
+docker build -t wordpress srcs/wordpress
+docker build -t ftps srcs/ftps
+docker build -t influxdb srcs/influxdb
+docker build -t grafana srcs/grafana
 
 #apply yaml files
 kubectl apply -f srcs/yamls
